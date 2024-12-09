c4566629dee389c89019814d98f3960d5b13f313
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-12-09 13:48:14 +0100

    Svg icons

---
+++ src/assets/Timer.jsx
@@ -0,0 +1,9 @@
+const timerIcon = () => {
+    return ( 
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 13V9M21 6L19 4M10 2H14M12 21C7.58172 21 4 17.4183 4 13C4 8.58172 7.58172 5 12 5C16.4183 5 20 8.58172 20 13C20 17.4183 16.4183 21 12 21Z" stroke="#F09136" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+     );
+}
+ 
+export default timerIcon;
