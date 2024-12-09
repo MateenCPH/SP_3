35828d869a3e5e2728f37c7175c4f941cbb93db9
MateenCPH <cph-mr645@cphbusiness.dk>
2024-12-09 12:10:48 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      Her er vores header med søgefelt og forskellige knapper
+    </header>
+      <h1>Meals app</h1>
+      <p>Her</p>
+      <p>er</p>
+      <p>en</p>
+      <p>hel</p>
+      <p>masse</p>
+      <p>måltider</p>
+    </>
+  )
+}
+
+export default App
