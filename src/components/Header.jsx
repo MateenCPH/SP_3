17b3a36e0a7cd23a101f98522651a1d66c9c83ee
MateenCPH <cph-mr645@cphbusiness.dk>
2024-12-09 14:32:13 +0100

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,34 @@
-import headerLogo from "../../public/header-logo.png";
+import HomeLogo from "../assets/homeLogo";
+import SearchLogo from "../assets/SearchLogo";
+import UserLogo from "../assets/UserLogo";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [burger, setBurger] = useState("");
+
+  const handleChange = (e) => {
+    setBurger(e.target.value);
+    console.log(burger);
+  };
+
   return (
     <>
-      <header className="mb-2 mt-2 flex flex-row">
+      <header className="mb-2 mt-2 flex flex-row justify-between">
         <Link to="/">
-          <img className="h-8 bg-Theme" src={headerLogo} alt="headerLogo" />
+          <HomeLogo className="h-8 bg-Theme" />
         </Link>
-        <input
-          className="p-2 w-[20%] rounded bg-Secondary"
-          type="text"
-          name="search"
-          placeholder="Search..."
-        />
-        
+        <div className="p-2 rounded bg-Secondary flex flex-row">
+          <input
+            className="bg-transparent"
+            type="text"
+            name="search"
+            placeholder="Search..."
+            onChange={handleChange}
+          />
+          <SearchLogo />
+        </div>
+          <UserLogo />
       </header>
     </>
   );
