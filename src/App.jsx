41642fe2cc744acaa087e0fc3841c6155fcb7ce4
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-12-10 12:01:11 +0100

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchData } from "./util/persistence";
+
+import "./App.css";
+
 import Admin from "./pages/Admin";
 import Details from "./pages/Details";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetchData("https://meals.nerdshub.dk/api/meals", setMeals, "GET");
+  }, []);
+
   return (
     <>
     <div className="max-w-[63rem] m-auto p-2">
       <Router>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Homepage meals={meals} />} />
           <Route path="/details/:id" element={<Details />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<Admin />} />
