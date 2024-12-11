9c7a1897204688e5de9daf35d878a04aaa87d9b1
MateenCPH <cph-mr645@cphbusiness.dk>
2024-12-11 21:17:29 +0100

    implemented a fancy button

--- src/pages/Error404NotFound.jsx
+++ src/pages/Error404NotFound.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import QuestionMark from "../assets/QuestionMark";
 
 function Error404NotFound() {
   const location = useLocation();
+  const [msg, setMsg] = useState();
+
+  const handleClick = () => {
+    setMsg(
+      <>
+        <div className="flex flex-row">
+          <p className="mt-5 text-xl">
+            Dude, why did you click the gay button?
+          </p>
+          <button className="bg-Theme p-2 rounded-md mt-4">
+            Click this button instead
+          </button>
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
@@ -15,11 +32,16 @@ function Error404NotFound() {
           Sorry, the page: {location.pathname} you are looking for does not
           exist{" "}
         </p>
+        {/* <Link to={"/"}> */}
+        <button onClick={handleClick} className="bg-Theme p-2 rounded-md mt-4">
+          Gay button
+        </button>
+
         <Link to={"/"}>
-          <button className="bg-Theme p-2 rounded-md mt-4">
-            Gay button
-          </button>
+          <span>{msg}</span>
         </Link>
+
+        {/* </Link> */}
       </div>
     </>
   );
