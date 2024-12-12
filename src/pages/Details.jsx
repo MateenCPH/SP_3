35d184586002cfc025c03080fa628a1e3997da49
MateenCPH <cph-mr645@cphbusiness.dk>
2024-12-12 02:00:46 +0100

    details about meal is now able to render through pure URL search

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,51 +1,62 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { getImageForMeal } from "../components/MealsCard";
+import { useState, useEffect } from "react";
+import { fetchData } from "../util/persistence";
+import Error404NotFound from "./Error404NotFound";
 
 function Details() {
+  const { id } = useParams();
   const location = useLocation();
-  const { meal } = location.state || {};
+  const [meal, setMeal] = useState(location.state?.meal || null);
 
+  useEffect(() => {
+    if (!meal) {
+      fetchData(
+        "https://meals.nerdshub.dk/api/meals",
+        (data) => {
+          const selectedMeal = data.find((m) => String(m.mealId) === id);
+          setMeal(selectedMeal || null);
+        },
+        "GET"
+      );
+    }
+  }, [meal]);
+
+  if (!meal) {
+    return <Error404NotFound />;
+  }
   return (
     <>
-      {meal && meal.ingredients ? (
-        <>
-          <h1 className="text-center text-4xl my-8">{meal.mealName}</h1>
+      <h1 className="text-center text-4xl my-8">{meal.mealName}</h1>
 
-          <main className="flex justify-around text-center flex-col md:flex-row md:text-left bg-Secondary w-[100%] h-[80vh] p-4">
-            <div className="text-2xl mb-8">
-              <h2>Description</h2>
-              <p className="text-base">{meal.mealDescription}</p>
-              <br />
+      <main className="flex justify-around text-center flex-col md:flex-row md:text-left bg-Secondary w-[100%] h-[80vh] p-4">
+        <div className="text-2xl mb-8">
+          <h2>Description</h2>
+          <p className="text-base">{meal.mealDescription}</p>
+          <br />
 
-              <h2>Ingredients</h2>
+          <h2>Ingredients</h2>
 
-              {meal &&
-                meal.ingredients.map((ingredients) => (
-                  <p className=" text-base" key={ingredients.id}>
-                    {ingredients.name} - {ingredients.quantity}
-                  </p>
-                ))}
+          {meal &&
+            meal.ingredients.map((ingredients) => (
+              <p className=" text-base" key={ingredients.id}>
+                {ingredients.name} - {ingredients.quantity}
+              </p>
+            ))}
 
-              <br />
+          <br />
 
-              <h2>Instructions</h2>
-              <p className="text-base">{meal.mealInstructions}</p>
-            </div>
-            <div className="flex justify-center">
-              <img
-                className="md:w-[60vw] h-[30vh] object-cover rounded-sm"
-                src={getImageForMeal(meal.mealName)}
-                alt="Image of the food"
-              />
-            </div>
-          </main>
-        </>
-      ) : (
-        <>
-          <h1 className="text-center text-4xl my-8 bg-Primary h-[2.25rem]"></h1>
-          <main className=" bg-Secondary w-[100%] h-[80vh] p-4"></main>
-        </>
-      )}
+          <h2>Instructions</h2>
+          <p className="text-base">{meal.mealInstructions}</p>
+        </div>
+        <div className="flex justify-center">
+          <img
+            className="md:w-[60vw] h-[30vh] object-cover rounded-sm"
+            src={getImageForMeal(meal.mealName)}
+            alt="Image of the food"
+          />
+        </div>
+      </main>
     </>
   );
 }
