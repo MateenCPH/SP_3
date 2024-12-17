66074a00c90074b804c82df499e9030e0367f8a1
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-12-17 13:30:54 +0100

    Close Icon added

---
+++ src/assets/icons/CloseIcon.jsx
@@ -0,0 +1,9 @@
+const closeIcon = () => {
+    return ( 
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+     );
+}
+ 
+export default closeIcon;
